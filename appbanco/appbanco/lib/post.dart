abd1e7e380ad3855852755597059c413d081be5b
magaieskipedro <magaieskipedro@gmail.com>
2025-11-03 11:23:27 -0300

    api

---
+++ appbanco/appbanco/lib/post.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class PostPage extends StatefulWidget {
+  const PostPage({super.key});
+
+  @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  //aqui é a logica
+  TextEditingController novatemperatura = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    postValue();
+  }
+
+  String? erro;
+
+  Future<void> postValue() async {
+    try {
+      FirebaseFirestore.instance.collection("monitoramento").add({
+        // Tipo um JSON
+        "temperatura": novatemperatura.text,
+      });
+    } catch (err) {
+      setState(() {
+        //se der erro, o valor de erro altera
+        erro = "Erro ao enviar temperatura";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("PostPage")),
+        body: Center(
+          child: Column(
+            children: [
+              Text("Insira a sua temperatura"),
+              TextField(controller: novatemperatura),
+              ElevatedButton(onPressed: postValue, child: Text("Enviar")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
